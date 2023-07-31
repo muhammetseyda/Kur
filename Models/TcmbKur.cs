0ed2bc0c22f24a065ab49cc0328a0fba74b1b71d
Muhammet Seyda Armağan <muhammedseyda@hotmail.com>
2023-07-31 18:32:22 +0300

    Fix

--- Models/TcmbKur.cs
+++ Models/TcmbKur.cs
@@ -2,7 +2,7 @@
 {
     public class TcmbKur
     {
-        public int? Id { get; set; }
+        public int Id { get; set; }
         public string Kur { get; set; }
         public double Deger { get; set; }
         public DateTime Tarih { get; set; }
